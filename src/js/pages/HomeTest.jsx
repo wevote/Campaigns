14d9685b52d113bdd708abfd283bb039e6f2e134
dalemcgrew <DaleMcGrew@yahoo.com>
2021-01-31 06:21:03 -0800

    Added PageHeader to /hometest file.

--- src/js/pages/HomeTest.jsx
+++ src/js/pages/HomeTest.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import home from '../../img/demo/home.png';
+import PageHeader from '../components/Navigation/PageHeader';
 
 export default class HomeTest extends Component {
   render () {
@@ -8,6 +9,7 @@ export default class HomeTest extends Component {
 
     return (
       <div>
+        <PageHeader />
         <h1>This is the Home  page!</h1>
         <img src={home} alt="World" />
       </div>
