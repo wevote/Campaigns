15bd7005129d3c0f234d4dd910b4f6d640232999
stevepodell <steve@podell.com>
2021-02-03 10:37:21 -0800

    Got params working for CampaignDetailsPage We have a no abbreviations code policy, but we build cryptic urls.  Changed '/c/' to a suggestion for a human readable token.

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ class App extends Component {
   // See https://reactjs.org/docs/error-boundaries.html
   static getDerivedStateFromError (error) { // eslint-disable-line no-unused-vars
     // Update state so the next render will show the fallback UI, We should have a "Oh snap" page
-    console.log("App caught error", error);
+    console.log('App caught error', error);
     return { hasError: true };
   }
 
